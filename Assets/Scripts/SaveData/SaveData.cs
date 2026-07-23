14b8e1aef843870be74345e7efe8a2fcb3d81cfe
Salmon306 <darkquan306@gmail.com>
2026-07-23 16:28:06 +0700

    Thêm cơ chế save

---
+++ Assets/Scripts/SaveData/SaveData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public int stageIndex;      // Jack = 0, Queen = 1, King = 2
+    public int bossIndex;       // Boss tiếp theo cần đánh
+}
---
+++ Assets/Scripts/SaveData/SaveManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.IO;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance;
+
+    private string SavePath => Path.Combine(Application.persistentDataPath, "save.json");
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SaveProgress(int stageIndex, int bossIndex)
+    {
+        SaveData data = new SaveData()
+        {
+            stageIndex = stageIndex,
+            bossIndex = bossIndex
+        };
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(SavePath, json);
+
+        Debug.Log("Auto Save Success");
+    }
+
+    public SaveData LoadProgress()
+    {
+        if (!File.Exists(SavePath))
+            return null;
+
+        string json = File.ReadAllText(SavePath);
+        return JsonUtility.FromJson<SaveData>(json);
+    }
+
+    public void DeleteSave()
+    {
+        if (File.Exists(SavePath))
+            File.Delete(SavePath);
+    }
+
+    public bool HasSave()
+    {
+        return File.Exists(SavePath);
+    }
+}
